924fb709a14f3c090f1dbffd4cae8156c15e667e
srisairaaj10 <srisairaajs.it2020@citchennai.net>
2024-01-27 12:27:43 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!machi')
 })
 
 app.get('/status', (req, res) => {
